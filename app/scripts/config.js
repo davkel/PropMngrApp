3ddd767f605d3a828f26096eec761506832853cf
davkel <david.kelly37@mydit.ie>
2016-02-08 22:14:35 +0000

    init commit
    init

---
+++ app/scripts/config.js
@@ -0,0 +1,99 @@
+/**
+ * HOMER - Responsive Admin Theme
+ * Copyright 2015 Webapplayers.com
+ *
+ */
+
+function configState($stateProvider, $urlRouterProvider, $compileProvider) {
+
+    // Optimize load start with remove binding information inside the DOM element
+    $compileProvider.debugInfoEnabled(true);
+
+    // Set default state
+    $urlRouterProvider.otherwise("/dashboard");
+    $stateProvider
+
+        // Dashboard - Main page
+        .state('dashboard', {
+            url: "/dashboard",
+            templateUrl: "views/dashboard.html",
+            data: {
+                pageTitle: 'Dashboard',
+            }
+        })
+        // App views
+        .state('app_views', {
+            abstract: true,
+            url: "/app_views",
+            templateUrl: "views/common/content.html",
+            data: {
+                pageTitle: 'App Views'
+            }
+        })
+        .state('app_views.contacts', {
+            url: "/contacts",
+            templateUrl: "views/app_views/contacts.html",
+            data: {
+                pageTitle: 'Tenants',
+                pageDesc: 'List of all tenants'
+            }
+        })
+        
+        // Common views
+        .state('common', {
+            abstract: true,
+            url: "/common",
+            templateUrl: "views/common/content_empty.html",
+            data: {
+                pageTitle: 'Common'
+            }
+        })
+        .state('common.login', {
+            url: "/login",
+            templateUrl: "views/common_app/login.html",
+            data: {
+                pageTitle: 'Login page',
+                specialClass: 'blank'
+            }
+        })
+        .state('common.register', {
+            url: "/register",
+            templateUrl: "views/common_app/register.html",
+            data: {
+                pageTitle: 'Register page',
+                specialClass: 'blank'
+            }
+        })
+        .state('common.error_one', {
+            url: "/error_one",
+            templateUrl: "views/common_app/error_one.html",
+            data: {
+                pageTitle: 'Error 404',
+                specialClass: 'blank'
+            }
+        })
+        .state('common.error_two', {
+            url: "/error_two",
+            templateUrl: "views/common_app/error_two.html",
+            data: {
+                pageTitle: 'Error 505',
+                specialClass: 'blank'
+            }
+        })
+        .state('common.lock', {
+            url: "/lock",
+            templateUrl: "views/common_app/lock.html",
+            data: {
+                pageTitle: 'Lock page',
+                specialClass: 'blank'
+            }
+        })
+        
+}
+
+angular
+    .module('homer')
+    .config(configState)
+    .run(function($rootScope, $state) {
+        $rootScope.$state = $state;
+    });
